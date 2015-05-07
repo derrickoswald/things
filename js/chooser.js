2cc4c3c12f6cb6c989dad4dbcefe4e1c7409cf43
Derrick Oswald <derrick.oswald@gmail.com>
2015-05-07 22:39:48 +0200

    chooser improvements

--- js/chooser.js
+++ js/chooser.js
@@ -1,13 +1,29 @@
+/**
+ * @fileOverview Multi-text input field component.
+ * @name chooser
+ * @author Derrick Oswald
+ * @version: 1.0
+ */
 define
 (
     ["mustache"],
     function (mustache)
     {
-        function Chooser (target_list, label, placeholder)
+        /**
+         * Object that makes a chooser field set.
+         * @param {string} target_list DOM element name to contain the input set
+         * @param {string} label User visible label for the input set
+         * @param {string} placeholder User visible placeholder text in an empty input field
+         * @param {array} choices list of user visible items for the drop down menu
+         * (if not supplied or empty, the input field is a plain text input with no dropdown)
+         */
+        function Chooser (target_list, label, placeholder, choices)
         {
             /**
-             * DOM element name for entire input set.
+             * Unique DOM element name for entire input set.
              * The DIV with this id is filled with the chooser from the template.
+             * This is also used as an id pattern for this input set.
+             * This is suffixed with the index value to create unique element ids for input elements.
              */
             this.list_name = target_list;
 
@@ -38,10 +54,10 @@ define
              * List of predefined values made available in the drop-down list.
              * Each entry is a string.
              */
-            this.values = [];
+            this.values = choices;
 
             /**
-             * DOM element attribute name that stores the index value of the DOM set.
+             * DOM element attribute name that stores the index value of the input group.
              */
             this.data_source = "data_source";
 
@@ -50,76 +66,79 @@ define
              */
             this.data_target = "data_target";
 
-            /**
-             * Unique id pattern for this input set.
-             * This is suffixed with the index value to create a unique element id.
-             */
-            this.id_pattern = "input_";
-
-            var l = this.input_label;
-            this.label = function ()
-            {
-                return (("1" == this.index) ? l : "");
-            }
-
-            this.buttonclass = function ()
-            {
-                return (("1" == this.index) ? "add_item" : "remove_item");
-            }
-
             this.glyph = function ()
             {
                 return (("1" == this.index) ? "glyphicon-plus" : "glyphicon-minus");
             }
 
+            /**
+             * Mustache template to generate the list item DOM elements.
+             */
             this.template =
                  "{{#items}}" +
-                    "<label for='" + this.id_pattern + "{{index}}' " + this.data_source + "='{{index}}' class='col-sm-3 control-label'>" +
-                        "{{label}}" +
+                    "<label class='col-sm-3 control-label chooser-label' for='" + this.list_name + "_{{index}}' " + this.data_source + "='{{index}}'>" +
+                        this.input_label +
                     "</label>" +
                     "<div class='col-sm-9' " + this.data_source + "='{{index}}'>" +
                         "<div class='input-group'>" +
-                            "{{#values}}" +
+                            "{{#hasvalues}}" +
                                 "<span class='dropdown'>" +
-                                    "<input id='" + this.id_pattern + "{{index}}' type='text' class='form-control dropdown-toggle' " + this.data_source + "='{{index}}' data-toggle='dropdown' placeholder='" + this.prompt + "' aria-label='" + this.input_label + "' value='{{value}}'>" +
-                                    "<ul class='dropdown-menu pull-right' role='menu' aria-labelledby='" + this.id_pattern + "{{index}}' >" +
+                                    "<input id='" + this.list_name + "_{{index}}' type='text' class='form-control dropdown-toggle' " + this.data_source + "='{{index}}' data-toggle='dropdown' placeholder='" + this.prompt + "' aria-label='" + this.input_label + "' value='{{value}}'>" +
+                                    "<ul class='dropdown-menu pull-right' role='menu' aria-labelledby='" + this.list_name + "_{{index}}' >" +
                                         "{{#values}}" +
                                         "<li role='presentation'>" +
-                                            "<a " + this.data_source + "='" + this.id_pattern + "{{index}}' role='menuitem' tabindex='-1' href='#'>{{.}}</a>" +
+                                            "<a " + this.data_target + "='" + this.list_name + "_{{index}}' role='menuitem' tabindex='-1' href='#'>{{.}}</a>" +
                                         "</li>" +
                                         "{{/values}}" +
                                     "</ul>" +
                                 "</span>" +
-                            "{{/values}}" +
+                            "{{/hasvalues}}" +
                             "{{^values}}" +
-                                "<input id='" + this.id_pattern + "{{index}}' type='text' class='form-control' " + this.data_source + "='{{index}}' placeholder='" + this.prompt + "' aria-label='" + this.input_label + "' value='{{value}}'>" +
+                                "<input id='" + this.list_name + "_{{index}}' type='text' class='form-control' " + this.data_source + "='{{index}}' placeholder='" + this.prompt + "' aria-label='" + this.input_label + "' value='{{value}}'>" +
                             "{{/values}}" +
-                            "<span class='input-group-addon btn btn-default {{buttonclass}}' " + this.data_source + "='{{index}}'>" +
+                            "<span class='input-group-addon btn btn-default' " + this.data_source + "='{{index}}'>" +
                                 "<i class='glyphicon {{glyph}}'></i>" +
                             "</span>" +
                         "</div>" +
                     "</div>" +
                 "{{/items}}";
 
+            /**
+             * Context for mustache rendering.
+             */
             this.context =
             {
                 items: this.items, // list of user entered values
-                values: [], // list of pre-composed values for the drop-down list
-                label: this.label,
-                buttonclass: this.buttonclass,
+                values: this.values, // list of pre-composed values for the drop-down list
+                hasvalues: ("undefined" != typeof (this.values)), // boolean to turn on values processing
                 glyph: this.glyph
             };
         }
 
         /**
-         * Event handler for user entering a value by key entry.
+         * Event handler for user pressing Enter in an input field.
+         * @param {object} event keypress event from the input element
          */
-        Chooser.prototype.changed = function (context, event)
+        Chooser.prototype.enter = function (event)
+        {
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if (keycode == '13')
+            {
+                this.changed (event);
+                this.add (null);
+            }
+        }
+
+        /**
+         * Event handler for user entering a value by key entry.
+         * @param {object} event change event from the input element
+         */
+        Chooser.prototype.changed = function (event)
         {
             var index;
 
             index = event.target.getAttribute (this.data_source);
-            context.items.forEach
+            this.context.items.forEach
             (
                 function (item)
                 {
@@ -131,8 +150,9 @@ define
 
         /**
          * Event handler for drop-down list item selected.
+         * @param {object} event click event from the link list item
          */
-        Chooser.prototype.clicked = function (context, event)
+        Chooser.prototype.clicked = function (event)
         {
             var link;
             var target;
@@ -141,12 +161,12 @@ define
             link = event.target;
 
             // fill in the input field with the chosen drop-down list item
-            target = document.getElementById (link.getAttribute (data_target));
+            target = document.getElementById (link.getAttribute (this.data_target));
             target.value = link.innerHTML;
 
             // update the value list
-            index = target.getAttribute (data_source);
-            context.items.forEach
+            index = target.getAttribute (this.data_source);
+            this.context.items.forEach
             (
                 function (item)
                 {
@@ -157,15 +177,32 @@ define
         }
 
         /**
-         * Event handler for clicking the plus icon.
+         * Set the focus to the given input item.
+         * @param {number} index The index (data_source attribute value) of the item to focus on
          */
-        Chooser.prototype.add = function (context, event)
+        Chooser.prototype.focus = function (index)
+        {
+            var list;
+            var inputs;
+
+            list = document.getElementById (this.list_name);
+            inputs = list.getElementsByTagName ("input");
+            for (var i = 0; i < inputs.length; i++)
+                if (index == Number (inputs[i].getAttribute (this.data_source)))
+                    inputs[i].focus ();
+        }
+
+        /**
+         * Event handler for clicking the plus icon.
+         * @param {object} event click event from the input group addon item
+         */
+        Chooser.prototype.add = function (event)
         {
             var index;
 
             // find next index
             index = 0;
-            context.items.forEach
+            this.context.items.forEach
             (
                 function (item)
                 {
@@ -175,12 +212,20 @@ define
             );
 
             // add the next input element
-            context.items.push ({ index: index + 1, value: "" });
+            this.context.items.push ({ index: index + 1, value: "" });
 
-            this.render (context);
+            // update the DOM
+            this.render ();
+
+            // set focus to the new input element
+            this.focus (index + 1);
         }
 
-        Chooser.prototype.remove = function (context, event)
+        /**
+         * Event handler for clicking the minus icon.
+         * @param {object} event click event from the input group addon item
+         */
+        Chooser.prototype.remove = function (event)
         {
             var index;
             var list;
@@ -192,7 +237,7 @@ define
 
             // remove it from the list
             list = [];
-            context.items.forEach
+            this.context.items.forEach
             (
                 function (item)
                 {
@@ -200,21 +245,25 @@ define
                         list.push (item);
                 }
                 );
-                context.items = list;
+                this.context.items = list;
 
-                this.render (context);
+                this.render ();
             }
 
-        Chooser.prototype.render = function (context)
+        /**
+         * Render the chooser field set.
+         */
+        Chooser.prototype.render = function ()
         {
             var list;
-            var inputs;
-            var items;
-            var spans;
             var change;
+            var pressed;
+            var inputs;
             var click;
+            var items;
             var added;
             var removed;
+            var spans;
 
             // ensure there is at least one item to render
             if (0 == this.context.items.length)
@@ -225,26 +274,34 @@ define
             list.innerHTML = mustache.render (this.template, this.context);
 
             // handle edit events
-            change = this.changed.bind (this, context);
+            change = this.changed.bind (this);
+            pressed = this.enter.bind (this);
             inputs = list.getElementsByTagName ("input");
             for (var i = 0; i < inputs.length; i++)
+            {
                 inputs[i].addEventListener ("change", change);
+                inputs[i].addEventListener ("keypress", pressed);
+            }
 
             // handle drop down chosen events
-            click = this.clicked.bind (this, context);
+            click = this.clicked.bind (this);
             items = list.getElementsByTagName ("a");
             for (var i = 0; i < items.length; i++)
                 items[i].addEventListener ("click", click);
 
             // handle add and remove events on the input group addon button
-            added = this.add.bind (this, context);
-            removed = this.remove.bind (this, context);
+            added = this.add.bind (this);
+            removed = this.remove.bind (this);
             spans = list.getElementsByTagName ("span");
             for (var i = 0; i < spans.length; i++)
                 if (spans[i].classList.contains ("input-group-addon"))
                     spans[i].addEventListener ("click", (1 == Number (spans[i].getAttribute (this.data_source))) ? added : removed);
         }
 
-        return ({Chooser: Chooser });
+        return (
+            {
+                Chooser: Chooser
+            }
+        );
     }
 )
--- js/thingmaker/make.js
+++ js/thingmaker/make.js
@@ -7,56 +7,15 @@ define
 
         var author_chooser = null;
 
-        var tag_chooser = null;
-
-        /**
-         * Inner template for each license input field added on the form.
-         * Initially there are none, but in the init() function the first empty one is added.
-         */
-        var license_template =
-            "{{#licenses}}" +
-                "<label for='license-list-{{index}}' data-license='{{index}}' class='col-sm-3 control-label'>" +
-                    "{{label}}" +
-                "</label>" +
-                "<div class='col-sm-9' data-license='{{index}}'>" +
-                    "<div class='input-group'>" +
-                        "<span class='dropdown'>" +
-                            "<input id='license-list-{{index}}' type='text' class='form-control dropdown-toggle' data-license='{{index}}' data-toggle='dropdown' placeholder='License name' aria-label='tracker' value='{{license}}'>" +
-                            "<ul class='dropdown-menu pull-right' role='menu' aria-labelledby='license-list-{{index}}' >" +
-                                "{{#license_list}}" +
-                                "<li role='presentation'>" +
-                                    "<a class='license' data-target='license-list-{{index}}' role='menuitem' tabindex='-1' href='#'>{{.}}</a>" +
-                                "</li>" +
-                                "{{/license_list}}" +
-                            "</ul>" +
-                        "</span>" +
-                        "<span class='input-group-addon btn btn-default {{buttonclass}}' data-license='{{index}}'>" +
-                            "<i class='glyphicon {{glyph}}'></i>" +
-                        "</span>" +
-                    "</div>" +
-                "</div>" +
-            "{{/licenses}}";
-
-        function label ()
-        {
-            return (("1" == this.index) ? "Licenses" : "");
-        }
-
-        function buttonclass ()
-        {
-            return (("1" == this.index) ? "add_license" : "remove_license");
-        }
-
-        function glyph ()
-        {
-            return (("1" == this.index) ? "glyphicon-plus" : "glyphicon-minus");
-        }
+        var license_chooser = null;
 
         var license_list =
         [
             "Creative Commons Attribution 4.0 International License",
             "Open Hardware Initiative 2.0"
         ];
+
+        var tag_chooser = null;
 
         // show the contents of the encoded Thing as a link in the content area
         function showlink (torrent)
@@ -122,113 +81,10 @@ define
             );
         };
 
-        function license_changed (event, data)
-        {
-            var index;
-
-            // update the license list
-            index = event.target.getAttribute ("data-license");
-            data.context.licenses.forEach (function (item) { if (index == item.index) item.license = event.target.value; });
-        }
-
-        function license_clicked (event, data)
-        {
-            var link;
-            var target;
-            var index;
-
-            link = event.target;
-
-            // fill in the input field with the chosen license name
-            target = document.getElementById (link.getAttribute ("data-target"));
-            target.value = link.innerHTML;
-
-            // update the license list
-            index = target.getAttribute ("data-license");
-            data.context.licenses.forEach (function (item) { if (index == item.index) item.license = target.value; });
-        }
-
-        function render_licenses (data)
-        {
-            var list;
-            var change;
-            var inputs;
-            var click;
-            var licenses;
-            var add;
-            var remove;
-            var spans;
-
-            // re-render inject the new elements into the DOM
-            list = document.getElementById ("licenses");
-            list.innerHTML = mustache.render (license_template, data.context);
-
-            // handle edit events
-            change = function (event) { license_changed (event, data); };
-            inputs = list.getElementsByTagName ("input");
-            for (var i = 0; i < inputs.length; i++)
-                inputs[i].addEventListener ("change", change);
-
-            // handle drop down chosen events
-            click = function (event) { license_clicked (event, data); };
-            licenses = list.getElementsByTagName ("a");
-            for (var i = 0; i < licenses.length; i++)
-                licenses[i].addEventListener ("click", click);
-
-            // handle add and remove license events on the input group addon button
-            add = function (event) { add_license (event, data); }
-            remove = function (event) { remove_license (event, data); }
-            spans = list.getElementsByTagName ("span");
-            for (var i = 0; i < spans.length; i++)
-                if (spans[i].classList.contains ("input-group-addon"))
-                    spans[i].addEventListener ("click", (1 == Number (spans[i].getAttribute ("data-license"))) ? add : remove);
-        }
-
-        function add_license (event, data)
-        {
-            var index;
-
-            // find next index
-            index = 0;
-            data.context.licenses.forEach (function (item) { if (item.index > index) index = item.index; });
-
-            // add the next license input element
-            data.context.licenses.push ({ index: index + 1, license: "" });
-
-            render_licenses (data);
-        }
-
-        function remove_license (event, data)
-        {
-            var index;
-            var list;
-
-            // get the index
-            index = event.target.getAttribute ("data-license");
-            if (null == index)
-                index = event.target.parentElement.getAttribute ("data-license");
-
-            // remove it from the list
-            list = [];
-            data.context.licenses.forEach (function (item) { if (index != item.index) list.push (item); });
-            data.context.licenses = list;
-
-            // delete the DOM elements - could also re-render, but at added cost
-            $ ("label[data-license='" + index + "']").remove ();
-            $ ("div[data-license='" + index + "']").remove ();
-        }
-
         function init (event, data)
         {
-            data.context =
-            {
-                licenses: [],
-                license_list: license_list,
-                label: label,
-                buttonclass: buttonclass,
-                glyph: glyph
-            }
             author_chooser = new chooser.Chooser ("authors", "Authors", "Author");
+            license_chooser = new chooser.Chooser ("licenses", "Licenses", "License", license_list);
             tag_chooser = new chooser.Chooser ("tags", "Tags", "Tag");
             if (data.torrent && ((null == form_initialized_with) || (form_initialized_with != data.torrent._id)))
             {
@@ -247,7 +103,7 @@ define
                                 if (null != thing[id])
                                     if ("licenses" == id)
                                         for (var j = 0; j < thing[id].length; j++)
-                                            data.context.licenses.push ({ index: j + 1, license: thing[id][j] });
+                                            license_chooser.context.items.push ({ index: j + 1, license: thing[id][j] });
                                     else if ("authors" == id)
                                         for (var j = 0; j < thing[id].length; j++)
                                             author_chooser.context.items.push ({ index: j + 1, value: thing[id][j] });
@@ -260,11 +116,9 @@ define
                     }
                 }
             }
-            if (0 == data.context.licenses.length)
-                data.context.licenses.push ({ index: 1, license: "" });
-            render_licenses (data);
-            author_chooser.render (author_chooser.context);
-            tag_chooser.render (tag_chooser.context);
+            author_chooser.render ();
+            license_chooser.render ();
+            tag_chooser.render ();
         }
 
         return (
