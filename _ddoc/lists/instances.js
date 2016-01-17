4c72d4094151f585584a4ffd643210cbe657d3a5
Derrick Oswald <derrick.oswald@gmail.com>
2016-01-17 16:17:27 +0100

    instances URL

--- _ddoc/lists/instances.js
+++ _ddoc/lists/instances.js
@@ -9,9 +9,15 @@ function output (head, req)
             }
         }
     );
+    var vhosted = req.requested_path[0] == 'instances';
+    var root = vhosted ? '/root/' : '/'
     send ('<!DOCTYPE html>\n<html lang=\'en\'>\n    <body>\n');
-    send (toJSON (req.query));
-    send ('        <table>\n');
+    send ('<br>info:' + toJSON (req.info));
+    send ('<br>query:' + toJSON (req.query));
+    send ('<br>path:' + toJSON (req.path));
+    send ('<br>raw_path:' + toJSON (req.raw_path));
+    send ('<br>requested_path:' + toJSON (req.requested_path));
+    send ('<br>        <table>\n');
     send ('            <tr><th>Instance</th><th>Value</th></tr>\n');
     while (row = getRow ())
     {
@@ -20,21 +26,21 @@ function output (head, req)
         var db = row.value.public_database;
         send (''.concat(
             '            <tr>',
-            '<td><a href=\'/' + db + '/_design/' + db + '/_rewrite/root?instance_uuid=' + uuid + '&database=' + db + '\' target=_blank>' + name + ' ' + db + '</a></td>',
+            '<td><a href=\'' + root + db + '/_design/' + db + '/_rewrite/root?instance_uuid=' + uuid + '&database=' + db + '\' target=_blank>' + name + ' ' + db + '</a></td>',
             '<td>' + toJSON (row.value) + '</td>',
             '</tr>\n'
         ));
         db = row.value.pending_database;
         send (''.concat(
             '            <tr>',
-            '<td><a href=\'/' + db + '/_design/' + db + '/_rewrite/root?instance_uuid=' + uuid + '&database=' + db + '\' target=_blank>' + name + ' ' + db + '</a></td>',
+            '<td><a href=\'' + root + db + '/_design/' + db + '/_rewrite/root?instance_uuid=' + uuid + '&database=' + db + '\' target=_blank>' + name + ' ' + db + '</a></td>',
             '<td>' + toJSON (row.value) + '</td>',
             '</tr>\n'
         ));
         db = row.value.local_database;
         send (''.concat(
             '            <tr>',
-            '<td><a href=\'/' + db + '/_design/' + db + '/_rewrite/root?instance_uuid=' + uuid + '&database=' + db + '\' target=_blank>' + name + ' ' + db + '</a></td>',
+            '<td><a href=\'' + root + db + '/_design/' + db + '/_rewrite/root?instance_uuid=' + uuid + '&database=' + db + '\' target=_blank>' + name + ' ' + db + '</a></td>',
             '<td>' + toJSON (row.value) + '</td>',
             '</tr>\n'
         ));
